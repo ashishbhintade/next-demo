57a23777b4a70fc3e390f33c9ecd046a763b6067
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2023-07-28 09:56:35 +0530

    Me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import React, { useState } from "react";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
 
   const logout = async () => {
     try {
@@ -15,17 +17,37 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile Page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <hr />
       <button
         className=" mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={logout}
       >
         Logout
+      </button>
+
+      <button
+        className=" mt-4 bg-green-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={getUserDetails}
+      >
+        Get User Details
       </button>
     </div>
   );
